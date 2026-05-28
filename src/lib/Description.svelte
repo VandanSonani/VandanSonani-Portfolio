2f683f45048ec100be809ff5f5929147092639ed
VandanSonani <vsona2@uic.edu>
2026-05-28 09:09:04 -0500

    Fixed links

--- src/lib/Description.svelte
+++ src/lib/Description.svelte
@@ -90,7 +90,7 @@
                     <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
                 </svg>
             </a>
-            <a class="resume" href="https://drive.google.com/file/d/1snZcj-wXFLrIx4-9yZygHWLsQbnPRJ5b/view?usp=sharing" aria-label="x">
+            <a class="resume" href="https://drive.google.com/file/d/1ZswJ_vJe05GXgMkSNIzeMDpmBqo2Pi9k/view?usp=sharing" aria-label="x">
                 <svg viewBox="50 -10 510 500" height="37" width="40" xmlns="http://www.w3.org/2000/svg" class="resume-icon">
                     <path d="M288 48H136c-22.092 0-40 17.908-40 40v336c0 22.092 17.908 40 40 40h240c22.092 0 40-17.908 40-40V176L288 48zm-16 144V80l112 112H272z"></path></svg>
             </a>
